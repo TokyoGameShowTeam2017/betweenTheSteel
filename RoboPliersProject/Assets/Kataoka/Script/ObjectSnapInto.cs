1801fcd1d63acb364b41b375150073bfbc4ae27f
kataoka <ktok223@gmail.com>
2017-06-30 20:35:50 +0900

    リセット機能追加

--- RoboPliersProject/Assets/Kataoka/Script/ObjectSnapInto.cs
+++ RoboPliersProject/Assets/Kataoka/Script/ObjectSnapInto.cs
@@ -11,6 +11,8 @@ public class ObjectSnapInto : MonoBehaviour
     private Quaternion mQuaternion;
 
     private ArmManager mArm;
+
+    private float mCollisionTime;
     // Use this for initialization
     void Start()
     {
@@ -18,23 +20,33 @@ public class ObjectSnapInto : MonoBehaviour
         mQuaternion = transform.rotation;
 
         mArm = GameObject.FindGameObjectWithTag("ArmManager").GetComponent<ArmManager>();
+
+        mCollisionTime = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        int count = 0;
         foreach (var i in m_Collision)
         {
             if (i.GetComponent<ObjectCollision>().GetCollisionFlag()&&
                 mArm.GetEnablArmCatchingObject()==null||
                 i.transform.position.y<=-50.0f)
             {
-                GetComponent<Rigidbody>().velocity = Vector3.zero;
-                transform.position = mPosition;
-                transform.rotation = mQuaternion;
-
+                mCollisionTime += Time.deltaTime;
                 break;
             }
+            if (m_Collision.Length - 1 == count)
+                mCollisionTime = 0.0f;
+            count++;
+        }
+        if (mCollisionTime >= 3.0f)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            transform.position = mPosition;
+            transform.rotation = mQuaternion;
+            mCollisionTime = 0.0f;
         }
 
         //初期化
