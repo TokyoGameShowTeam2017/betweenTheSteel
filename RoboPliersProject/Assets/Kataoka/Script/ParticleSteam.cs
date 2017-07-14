3054c608fe6b80d568ec34a3f9f05b020cc3093e
kataoka <ktok223@gmail.com>
2017-07-14 14:54:40 +0900

    ぷしゅー実装

--- RoboPliersProject/Assets/Kataoka/Script/ParticleSteam.cs
+++ RoboPliersProject/Assets/Kataoka/Script/ParticleSteam.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 public class ParticleSteam : MonoBehaviour
 {
     private float mTime;
-   [SerializeField, Tooltip("どのぐらいの頻度")]
-
-
+    //ランダムタイム
+    private float mRandomTime;
+    [SerializeField, Tooltip("どのぐらいの頻度で")]
+    public float m_PushhTime;
+    [SerializeField, Tooltip("どのくらいの頻度でランダム値")]
+    public float m_PushhRandmTime;
 
     private ParticleSystem mParticle;
     // Use this for initialization
     void Start()
     {
         mParticle = GetComponent<ParticleSystem>();
+        mTime = 0.0f;
+        mRandomTime = Random.Range(-mRandomTime, mRandomTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+        mTime += Time.deltaTime;
 
+        if (mTime >= m_PushhTime + m_PushhRandmTime)
+        {
+            mTime = 0.0f;
+            mRandomTime = Random.Range(-mRandomTime, mRandomTime);
+            SoundManager.Instance.PlaySe("steam");
+            mParticle.Play();
+        }
     }
 }
