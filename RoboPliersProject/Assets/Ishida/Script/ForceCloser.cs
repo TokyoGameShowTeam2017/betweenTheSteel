8b2eb7fbadf07729d2e4310e84607a8dd6006c22
kataoka <ktok223@gmail.com>
2017-09-06 13:05:59 +0900

    タイトル戻る

--- RoboPliersProject/Assets/Ishida/Script/ForceCloser.cs
+++ RoboPliersProject/Assets/Ishida/Script/ForceCloser.cs
@@ -6,13 +6,15 @@ using UnityEngine.SceneManagement;
 public class ForceCloser : MonoBehaviour
 {
     // 操作が無い場合に自動で終了するまでの秒数
-    static readonly float AutoCloseDuration = 20;
+    static readonly float AutoCloseDuration = 120;
     // 最後に入力があった時間
     float lastInputTime;
 
-    private float m_PvTimer;
-    [SerializeField, Tooltip("何秒後にPVシーンに遷移するか")]
-    private float m_PvSceneTime;
+
+    float time=0.0f;
+    //private float m_PvTimer;
+    //[SerializeField, Tooltip("何秒後にPVシーンに遷移するか")]
+    //private float m_PvSceneTime;
 
     private enum State
     {
@@ -41,10 +43,14 @@ public class ForceCloser : MonoBehaviour
         DontDestroyOnLoad(gameObject);
         lastInputTime = Time.time;
 
-        m_PvTimer = 0;
+        //m_PvTimer = 0;
     }
     void Update()
     {
+        time += Time.deltaTime;
+        if (time <= 10.0f) {
+            return; 
+        }
         if (m_State == State.ExitState)
         {
             // XBOX コントローラーの BACK ボタンと START ボタン同時押し
@@ -70,59 +76,65 @@ public class ForceCloser : MonoBehaviour
         }
         else if (m_State == State.ResetState)
         {
-            if ((Input.GetKey(KeyCode.JoystickButton6) && Input.GetKey(KeyCode.JoystickButton7)))
+            if ((Input.GetKey(KeyCode.JoystickButton6) && Input.GetKey(KeyCode.JoystickButton7)) || Input.GetKeyDown(KeyCode.Space))
             {
+                GameObject obj = GameObject.Find("PlayerTutorial");
+                GameObject.Destroy(obj);
+                GameObject.Destroy(GameObject.Find("SceneLoadArea"));
+                GameObject.Destroy(GameObject.Find("SoundManager(Clone)"));
+                time = 0.0f;
                 SceneManager.LoadScene("title");
                 return;
             }
+
         }
         else if (m_State == State.PvStartState)
         {
 
-            if (SceneManager.GetActiveScene().name == "movie") return;
-            MigrationPvScene();
+            //if (SceneManager.GetActiveScene().name == "movie") return;
+            //MigrationPvScene();
 
-            // 何かしらの入力があれば、最後に入力があった時間として現在の時間を記録する
-            if (InputAnyKey())
-            {
-                lastInputTime = Time.time;
-            }
-            // 最後に入力があった瞬間からの経過時間が、指定秒数を超えたら、タイトル画面へ
-            if ((Time.time - lastInputTime) >= AutoCloseDuration)
-            {
-                if (!m_Onec)
-                {
-                    m_Onec = true;
+            //// 何かしらの入力があれば、最後に入力があった時間として現在の時間を記録する
+            //if (InputAnyKey())
+            //{
+            //    lastInputTime = Time.time;
+            //}
+            //// 最後に入力があった瞬間からの経過時間が、指定秒数を超えたら、タイトル画面へ
+            //if ((Time.time - lastInputTime) >= AutoCloseDuration)
+            //{
+            //    if (!m_Onec)
+            //    {
+            //        m_Onec = true;
 
-                    SceneManager.LoadScene("title");
-                }
-            }
+            //        SceneManager.LoadScene("title");
+            //    }
+            //}
         }
     }
 
 
-    //何秒かタイトルで放置したらPVシーンへ
-    public void MigrationPvScene()
-    {
-        if (GameObject.Find("SceneCollection") == null) return;
-        if (GameObject.Find("SceneCollection").GetComponent<SceneCollection>().GetSceneState() == 0)
-        {
-            m_PvTimer += Time.deltaTime;
-            if (m_PvTimer >= m_PvSceneTime)
-            {
-                Destroy(GameObject.Find("SoundManager(Clone)"));
-                Destroy(GameObject.Find("PlayerTutorial"));
-                Destroy(GameObject.Find("SceneLoadArea"));
+    ////何秒かタイトルで放置したらPVシーンへ
+    //public void MigrationPvScene()
+    //{
+    //    if (GameObject.Find("SceneCollection") == null) return;
+    //    if (GameObject.Find("SceneCollection").GetComponent<SceneCollection>().GetSceneState() == 0)
+    //    {
+    //        m_PvTimer += Time.deltaTime;
+    //        if (m_PvTimer >= m_PvSceneTime)
+    //        {
+    //            Destroy(GameObject.Find("SoundManager(Clone)"));
+    //            Destroy(GameObject.Find("PlayerTutorial"));
+    //            Destroy(GameObject.Find("SceneLoadArea"));
 
-                m_PvTimer = 0.0f;
-                SceneManager.LoadScene("movie");
-            }
-        }
-        else
-        {
-            m_PvTimer = 0;
-        }
-    }
+    //            m_PvTimer = 0.0f;
+    //            SceneManager.LoadScene("movie");
+    //        }
+    //    }
+    //    else
+    //    {
+    //        m_PvTimer = 0;
+    //    }
+    //}
 
 
 
